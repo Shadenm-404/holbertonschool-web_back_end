8dadea70e76fbbbb0dde237ccbfcd2d6b16ec692
Shaden Alalwani <viuxwl7@gmail.com>
2026-02-18 13:02:35 +0300

    Correct async_comprehension function signature
    Fix function signature by removing incorrect arrow.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ from importlib import import_module as using
 async_generator = using('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension()  List[float]:
     '''Creates a list of 10 numbers from a 10-number generator.
     '''
     return [num async for num in async_generator()]
