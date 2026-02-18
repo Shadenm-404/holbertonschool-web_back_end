4021870218b014d9c32350f0f5fbf4d8a1c3333e
Shaden Alalwani <viuxwl7@gmail.com>
2026-02-18 13:00:59 +0300

    Refactor async_generator for random number range
    Updated async_generator to yield random numbers between 0 and 9.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-"""
-This module contains an asynchronous generator.
-"""
-
+'''Task 0's module.
+'''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """
-    Yields 10 random numbers between 0 and 10,
-    waiting 1 second between each yield.
-    """
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 9
