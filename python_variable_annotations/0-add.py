254ff5cd56742c5ffac2393acf76c621c1938155
Shaden Alalwani <viuxwl7@gmail.com>
2026-02-18 12:34:06 +0300

    Fix pycodestyle formatting

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,8 +4,10 @@ This module contains a function that adds two floats
 and returns their sum.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Returns the sum of two float numbers.
     """
     return a + b
+
