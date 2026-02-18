e7684603e65aae771f123119731dfa6d05f59f75
Shaden Alalwani <viuxwl7@gmail.com>
2026-02-18 12:29:28 +0300

    Task 0: Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that adds two floats
+and returns their sum.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two float numbers.
+    """
+    return a + b
+
