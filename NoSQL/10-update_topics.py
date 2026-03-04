017adbe8bef25dbd79f043ac331931bc8c3692e1
Shaden Alalwani <viuxwl7@gmail.com>
2026-03-04 10:45:14 +0300

    Task 10 update topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Update school topics"""
+"""Update topics of a school"""
+
 
 def update_topics(mongo_collection, name, topics):
-    """Update topics based on school name"""
-    mongo_collection.update_many({
+    """changes all topics of a school document based on the name"""
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
+
