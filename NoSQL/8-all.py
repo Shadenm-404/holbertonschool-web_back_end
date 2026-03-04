80a12a24724b0282aa5e7dff15793d1a92d93e74
Shaden Alalwani <viuxwl7@gmail.com>
2026-03-04 10:31:48 +0300

    Add task 8 list_all function

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that lists all documents in a MongoDB collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Return a list of all documents in the collection
+    """
+    return list(mongo_collection.find())
