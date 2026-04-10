ea71686fb173ee537aef4c7791c0030659e09d8d
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-10 13:28:38 +0300

    Fix Task 1 properly

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?
-');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+if (!process.stdin.isTTY) {
+  process.on('exit', () => {
+    console.log('This important software is now closing');
+  });
+}
