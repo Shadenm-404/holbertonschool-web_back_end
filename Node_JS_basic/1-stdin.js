b419c05e685715c94ea97cc80d9423852e7ce8b4
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 15:27:39 +0300

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,23 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-let input = '';
-
-process.stdin.on('data', (chunk) => {
-  input += chunk;
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
-  input = input.trim();
-  if (input) {
-    console.log(`Your name is: ${input}`);
-  }
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
-
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (chunk) => {
-    const name = chunk.trim();
-    console.log(`Your name is: ${name}`);
-  });
-}
