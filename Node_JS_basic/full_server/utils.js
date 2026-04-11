9d0756a2934b62941864eff968309d456ded1671
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 15:09:07 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,29 +1,25 @@
-import fs from 'fs';
+const fs = require('fs');
 
-export default function readDatabase(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-        return;
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = {};
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (firstname && field) {
+        if (!students[field]) students[field] = [];
+        students[field].push(firstname);
       }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
-
-      const fields = {};
-
-      students.forEach((student) => {
-        const [firstname, , , field] = student.split(',');
-
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-
-        fields[field].push(firstname);
-      });
-
-      resolve(fields);
     });
+
+    resolve(students);
   });
-}
+});
+
+module.exports = { readDatabase };
