5828048a72cc033c5fca69b640aa195354ca503d
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 15:37:17 +0300

    Refactor StudentsController to improve readability

--- full_server/controllers/StudentsController.js
+++ full_server/controllers/StudentsController.js
@@ -2,20 +2,15 @@ import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(req, res) {
-    const filePath = process.argv[2];
-
-    readDatabase(filePath)
-      .then((students) => {
-        const sortedFields = Object.keys(students).sort((a, b) =>
-          a.toLowerCase().localeCompare(b.toLowerCase())
-        );
-
-        let response = 'This is the list of our students\n';
-        sortedFields.forEach((field) => {
-          response += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+    const dbPath = process.argv[2];
+    readDatabase(dbPath)
+      .then((data) => {
+        const sorted = Object.keys(data).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
+        let text = 'This is the list of our students\n';
+        sorted.forEach((field) => {
+          text += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
         });
-
-        res.status(200).send(response.trim());
+        res.status(200).send(text);
       })
       .catch(() => {
         res.status(500).send('Cannot load the database');
@@ -23,17 +18,15 @@ class StudentsController {
   }
 
   static getAllStudentsByMajor(req, res) {
-    const filePath = process.argv[2];
     const { major } = req.params;
-
     if (major !== 'CS' && major !== 'SWE') {
       res.status(500).send('Major parameter must be CS or SWE');
       return;
     }
-
-    readDatabase(filePath)
-      .then((students) => {
-        const list = students[major] || [];
+    const dbPath = process.argv[2];
+    readDatabase(dbPath)
+      .then((data) => {
+        const list = data[major] || [];
         res.status(200).send(`List: ${list.join(', ')}`);
       })
       .catch(() => {
