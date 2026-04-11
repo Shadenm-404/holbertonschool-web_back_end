cdb78c84f03dcef83fd9a185b6355f22c6b5e02b
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 15:58:16 +0300

    Task 1: stdin

--- 1-stdin.js
+++ 1-stdin.js
@@ -3,7 +3,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write('Your name is: ' + data.toString().trim() + '\n');
+  const name = data.toString().trim();
+  if (name) {
+    process.stdout.write('Your name is: ' + name + '\n');
+  }
 });
 
 process.stdin.on('end', () => {
