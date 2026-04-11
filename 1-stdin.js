b59eaf5f8dec84eba4306e4d90385692ed1cc68b
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 16:02:37 +0300

    Update 1-stdin.js

--- 1-stdin.js
+++ 1-stdin.js
@@ -1,19 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let input = '';
-
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (chunk) => {
-  input += chunk;
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  process.stdout.write('Your name is: ' + name + '\n');
 });
 
 process.stdin.on('end', () => {
-  const name = input.trim();
-
-  if (name) {
-    process.stdout.write('Your name is: ' + name + '\n');
-  }
-
   process.stdout.write('This important software is now closing\n');
 });
