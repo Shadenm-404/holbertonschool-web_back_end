4a5054bce65ffce924d9171a37bf4cbf9d44d78d
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 19:02:36 +0300

    Refactor server.js to improve import and logging

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import router from './routes/index';
+import router from './routes';
 
 const app = express();
+const port = 1245;
 
 app.use('/', router);
 
-app.listen(1245, () => {
-  console.log('Server running on port 1245');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 export default app;
