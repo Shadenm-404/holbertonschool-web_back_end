41c265e2ece68090543e4b0a8f684402634451b2
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 16:05:45 +0300

    Refactor stdin handling to read names continuously

--- 1-stdin.js
+++ 1-stdin.js
@@ -1,10 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+  let chunk;
+  while ((chunk = process.stdin.read()) !== null) {
+    const name = chunk.trim();
+    process.stdout.write('Your name is: ' + name + '\n');
   }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
