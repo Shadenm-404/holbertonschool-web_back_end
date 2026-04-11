0c7f7124ddfbad2be8cb511904411c8d5816c060
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 07:54:59 +0300

    Task 8: full server setup

---
+++ full_server/controllers/StudentsController.js
@@ -0,0 +1,43 @@
+import readDatabase from '../utils';
+
+export default class StudentsController {
+  static getAllStudents(req, res) {
+    const database = process.argv[2];
+
+    readDatabase(database)
+      .then((fields) => {
+        let output = 'This is the list of our students';
+
+        const sortedFields = Object.keys(fields).sort((a, b) =>
+          a.localeCompare(b, undefined, { sensitivity: 'base' })
+        );
+
+        sortedFields.forEach((field) => {
+          output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        });
+
+        res.status(200).send(output);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const database = process.argv[2];
+    const major = req.params.major;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(database)
+      .then((fields) => {
+        res.status(200).send(`List: ${fields[major].join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
---
+++ full_server/utils.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+export default function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+
+      students.forEach((student) => {
+        const [firstname, , , field] = student.split(',');
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      });
+
+      resolve(fields);
+    });
+  });
+}
