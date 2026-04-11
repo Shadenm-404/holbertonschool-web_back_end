76cde8310bca767f570bb2da608a5048d3f89b57
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 15:36:26 +0300

    Refactor getHomepage method parameters for clarity

--- full_server/controllers/AppController.js
+++ full_server/controllers/AppController.js
@@ -1,6 +1,10 @@
+import express from 'express';
+
+const app = express();
+
 class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello Holberton School!');
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
