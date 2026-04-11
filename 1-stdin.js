7e1fa8b6537f52ae7da09d5f6475984e5239bfc3
Shaden Alalwani <viuxwl7@gmail.com>
2026-04-11 15:50:56 +0300

    Task 1: stdin working

--- 1-stdin.js
+++ 1-stdin.js
@@ -1,18 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?
-');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-let input = '';
-
-process.stdin.on('data', (chunk) => {
-  input += chunk;
+process.stdin.on('data', (data) => {
+  process.stdout.write('Your name is: ' + data.toString().trim() + '\n');
 });
 
 process.stdin.on('end', () => {
-  const name = input.trim();
-  process.stdout.write('Your name is: ' + name + '
-');
-  process.stdout.write('This important software is now closing
-');
+  process.stdout.write('This important software is now closing\n');
 });
